a1afd326e6dad140d40e12162945a15e927f7f24
113833-PRADO <128556544+113833-PRADO@users.noreply.github.com>
2023-09-08 19:43:01 -0300

    1 file modified, 6 added

---
+++ src/usuario-1/usuario-1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-1',
+  templateUrl: './usuario-1.component.html',
+  styleUrls: ['./usuario-1.component.css']
+})
+export class Usuario1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/usuario-2/usuario-2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-2',
+  templateUrl: './usuario-2.component.html',
+  styleUrls: ['./usuario-2.component.css']
+})
+export class Usuario2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
